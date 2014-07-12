efa0f074200f67048cd359b30d4171c82e1655b6
Scub3d <scubedtechnologiesllc@gmail.com>
2014-07-12 04:09:51 -0400

    Shit was done

--- SAO_Interface_Leap/src/Button.java
+++ SAO_Interface_Leap/src/Button.java
@@ -4,8 +4,14 @@ import java.awt.Graphics2D;
 public abstract class Button {
 	
 	public boolean inLockState; // Allows a button to be in lock state
-	
 	public boolean inHighlightedState; // Changes whether the button is highlighted or not
+	public int xPos;
+	public int yPos;
+	public String type;
+	public String foregroundName;
+	public String foregroundBaseURL;
+	public String backgroundName;
+	public String backgroundBaseURL;
 	
 	public abstract void onCreation(Graphics2D g2);
 
